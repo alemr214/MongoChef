1d4c5546779238bb2ea83b5c8942c5882978abd8
alemr214 <ale.mr0214@gmail.com>
2025-04-24 15:27:29 -0600

    Add Pydantic model for user validation in users_schema.py

---
+++ app/schemas/users_schema.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Union
+
+
+class UsersBase(BaseModel):
+    """
+    Pydantic model for user validation.
+
+    Attributes:
+        - name: str
+        - lastname1: str
+        - lastname2: str | None
+        - email: EmailStr
+        - passwd: str
+    """
+
+    name: str = Field(min_length=1, max_length=70)
+    lastname1: str = Field(min_length=1, max_length=70)
+    lastname2: Union[str, None] = Field(default=None, max_length=70)
+    email: EmailStr = Field(min_length=1, max_length=150)
+    passwd: str = Field(min_length=8, max_length=50)
