7ecc85ad8e3b881496ccfe36d8262ea0258b6522
alemr214 <ale.mr0214@gmail.com>
2025-04-24 12:50:38 -0600

    Add Users model for MongoDB using Beanie ORM

---
+++ app/models/users_models.py
@@ -0,0 +1,24 @@
+from beanie import Document, Indexed
+from pydantic import EmailStr
+
+
+class Users(Document):
+    """
+    Users model extends from Document for MongoDB template with Beanie ORM.
+
+    Attributes:
+        - name: str
+        - lastname1: str
+        - lastname2: str | None
+        - email: EmailStr (unique=True)
+        - passwd: str
+    """
+
+    name: str
+    lastname1: str
+    lastname2: str | None
+    email: Indexed(EmailStr, unique=True)  # type: ignore
+    passwd: str
+
+    class Settings:
+        name = "users"
