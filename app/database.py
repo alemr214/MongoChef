cde257a3dca02ae37de9dda1f1926cee61d0eb30
alemr214 <ale.mr0214@gmail.com>
2025-04-24 15:31:15 -0600

    Add MongoDB connection setup with Beanie ORM in database.py

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from models.users_models import Users
+
+# MongoDB connection settings
+DATABASE_URL = "mongodb://localhost:27017"
+DATABASE_NAME = "mongochef"
+COLLECTIONS = [Users]  # Collections to use and create
+
+
+# Init connection to MongoDB with Beanie
+async def init() -> AsyncIOMotorClient:
+    """
+    Initialize the MongoDB connection and Beanie ORM.
+
+    Returns:
+        AsyncIOMotorClient: The MongoDB client instance.
+    """
+    client = AsyncIOMotorClient(DATABASE_URL)
+    db = client[DATABASE_NAME]
+    await init_beanie(database=db, document_models=COLLECTIONS)
+    return client  # Return the client for close use
