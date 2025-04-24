5456ad707296de57857d0aa85d1b1e2bed6671bc
alemr214 <ale.mr0214@gmail.com>
2025-04-24 15:27:52 -0600

    Add main application file with FastAPI setup and MongoDB connection

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from database import init
+from routers import users_routers
+from typing import AsyncGenerator, Any
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, Any]:
+    """
+    Lifespan event handler for the FastAPI application to make a connection with MongoDB using Beanie ORM.
+
+    Args:
+        app (FastAPI): FastAPI application instance.
+    """
+    app.state.mongo_client = await init()
+    yield
+    await app.state.mongo_client.close()
+
+
+# Instance with the initial settings
+app = FastAPI(
+    title="MongoChef API",
+    description="API built with FastAPI and MongoDB for a Recipe Management System.",
+    version="1.0.0",
+    docs_url="/docs",
+    lifespan=lifespan,  # Event handler for the lifespan of the app
+)
+
+app.include_router(users_routers.router, tags=["users"])
