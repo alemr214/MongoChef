1079d644516b336e715a3293cce564772a6cc334
alemr214 <ale.mr0214@gmail.com>
2025-04-24 18:13:38 -0600

    Add Categories model for recipe categories using Beanie ODM

---
+++ app/models/categories_model.py
@@ -0,0 +1,17 @@
+from beanie import Document, Indexed
+
+
+class Categories(Document):
+    """
+    Recipes category model extends from Document for MongoDB template with Beanie ORM.
+
+    Attributes:
+        - name: str
+        - description: str | None
+    """
+
+    name: Indexed(str, unique=True)  # type: ignore
+    description: str | None = None
+
+    class Settings:
+        name = "categories"
