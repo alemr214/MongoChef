4e3a7b3914807506165aaba0277edbaf6d320165
alemr214 <ale.mr0214@gmail.com>
2025-04-26 15:57:48 -0600

    Refactor Pydantic models for ingredients, kitchen tools, and categories to enhance clarity and structure

--- app/schemas/recipes_schema.py
+++ app/schemas/recipes_schema.py
@@ -1,40 +1,62 @@
 from pydantic import BaseModel, Field
-from datetime import timedelta
+from typing import List
 
 
-class IngredientsDetail(BaseModel):
+class IngredientsBaseDetail(BaseModel):
     """
-    IngredientsDetail model for detailed information about ingredients.
+    IngredientsBaseDetail is a Pydantic model that represents the details of an ingredient.
 
     Attributes:
-        - ingredients: Link[Ingredients]
-        - quantity: int | float
-        - unit: str
+        name (str): The name of the ingredient.
+        quantity (int): The quantity of the ingredient.
+        unit (str): The unit of measurement for the ingredient.
     """
 
-    ingredients: str  # Placeholder for Link[Ingredients]
-    quantity: int | float = Field(gt=0)
-    unit: str = Field(min_length=1, max_length=20)
+    name: str
+    quantity: int
+    unit: str
+
+
+class KitchenToolsBaseInfo(BaseModel):
+    """
+    KitchenToolsBaseInfo is a Pydantic model that represents the information of kitchen tools.
+
+    Attributes:
+        name (str): The name of the kitchen tools
+    """
+
+    name: str
+
+
+class CategoriesBaseInfo(BaseModel):
+    """
+    CategoriesBaseInfo is a Pydantic model that represents the information of recipe categories.
+
+    Attributes:
+        name (str): The name of the recipe category.
+    """
+
+    name: str
 
 
 class RecipesBase(BaseModel):
     """
-    Pydantic model for users validation in the endpoint
+    RecipesBase is a Pydantic model that represents the base information of a recipe.
 
     Attributes:
-        - title: str
-        - ingredients: list[IngredientsDetail]
-        - kitchen_tools: list[str]
-        - portions: int
-        - instructions: str
-        - cooking_time: int
-        - category: str
+        title (str): The title of the recipe.
+        ingredients (List[IngredientsBaseDetail]): A list of ingredients required for the recipe.
+        kitchen_tools (List[KitchenToolsBaseInfo]): A list of kitchen tools required for the recipe.
+        portions (int): The number of portions the recipe serves.
+        instructions (str): The instructions for preparing the recipe.
+        cooking_time (int): The time required to cook the recipe in minutes.
+        category (CategoriesBaseInfo): The category of the recipe.
     """
 
-    title: str = Field(min_length=1, max_length=70)
-    ingredients: list[IngredientsDetail]
-    kitchen_tools: list[str]  # Placeholder for Link[KitchenTools]
-    portions: int = Field(gt=0)
+    title: str = Field(min_length=1, max_length=50)
+    ingredients: List[IngredientsBaseDetail]
+    kitchen_tools: List[KitchenToolsBaseInfo]
+    portions: int = Field(gt=1)
     instructions: str = Field(min_length=1)
-    cooking_time: timedelta  # Placeholder for timedelta
-    category: str  # Placeholder for Link[Categories]
+    cooking_time: int = Field(gt=0)
+    category: CategoriesBaseInfo
